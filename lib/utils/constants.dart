3ed19cf89d44682e7353104a1faabdca7fc0d464
Gustavo <gustavogmfarias@gmail.com>
2023-02-27 18:33:07 -0300

    .

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const productBaseUrl =
+      'https://curso-projeto-31c16-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://curso-projeto-31c16-default-rtdb.firebaseio.com/orders';
+}
